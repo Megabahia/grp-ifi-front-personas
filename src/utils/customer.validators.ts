c178b98497e80d7e5f9c4d014545eeae41649820
james <jimmy1817@hotmail.com>
2022-12-29 23:32:37 -0500

    [Jimmy] correccion de validar ruc en solicitud de credito

--- src/utils/customer.validators.ts
+++ src/utils/customer.validators.ts
@@ -77,9 +77,12 @@ export class ValidacionesPropias {
 
     static rucValido(control: AbstractControl) {
         const valido = validateDocument.getValidateDocument('ruc', control.value);
+        let errors = control['errors'] || {};
         if (valido.status === 'SUCCESS') {
+            delete errors?.['rucInvalid'];
             return null;
         } else {
+            control.setErrors({...errors, rucInvalid: true});
             return {rucInvalid: true};
         }
     }
